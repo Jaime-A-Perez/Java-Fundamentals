45406aada06444b4afbb773f04664d50de20621d
Jaime-A-Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2025-08-09 16:22:18 -0500

    Loop for

---
+++ 02-primitive-data-type/src/loops/Loop.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class Loop {
+    public static void main(String[] args) {
+        // Bucle for
+        //for( int i = 1; i <= 5; i = i + 2 ) {
+        for( int i = 2; i <= 5; i+=2 ) {
+            System.out.println("Value: " + i);
+        }
+    }
+}
